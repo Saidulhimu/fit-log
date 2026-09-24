e0face6b56dbe160f8f7e58ba9e5b2c7338ab76b
Saidulhimu <saidulislam5595@gmail.com>
2026-09-25 01:18:36 +0600

    Project Environment setup and Breakdown project works

--- src/app/my-plan/page.tsx
+++ src/app/my-plan/page.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 
 const page = () => {
-  return (
-    <div>
-      home
-
-
-
-
-      hh
-    </div>
-  );
+    return (
+        <div>
+            my plan
+        </div>
+    );
 };
 
 export default page;
